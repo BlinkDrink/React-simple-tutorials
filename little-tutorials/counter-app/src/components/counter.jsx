23a522b097d281dc493a99a3a918e7ef7ef04df2
evrolan <evrolan1404@gmail.com>
2022-02-17 18:08:43 +0200

    Removing the local state of counter.jsx class. This way only <Counters/> handles all of the update,delete operations and has full control over them by passing onAction props to its children

--- little-tutorials/counter-app/src/components/counter.jsx
+++ little-tutorials/counter-app/src/components/counter.jsx
@@ -1,37 +1,34 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-	state = {
-		value: this.props.value,
-	};
-
 	render() {
 		return (
 			<div>
-				<h4>Counter #{this.props.id}</h4>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button
-					onClick={this.handleIncrement}
+					onClick={() => this.props.onIncrement(this.props.counter)}
 					className="btn btn-secondary btn-sm"
 				>
 					Increment
+				</button>
+				<button
+					onClick={() => this.props.onDelete(this.props.counter.id)}
+					className="btn btn-danger btn-sm m-2"
+				>
+					Delete
 				</button>
 			</div>
 		);
 	}
 
-	handleIncrement = () => {
-		this.setState({ value: this.state.value + 1 });
-	};
-
 	formatCount() {
-		const { value: count } = this.state;
+		const { value: count } = this.props.counter;
 		return count === 0 ? "Zero " : count;
 	}
 
 	getBadgeClasses() {
 		let classes = "badge m-2 bg-";
-		classes += this.state.value === 0 ? "warning" : "primary";
+		classes += this.props.counter.value === 0 ? "warning" : "primary";
 		return classes;
 	}
 }
