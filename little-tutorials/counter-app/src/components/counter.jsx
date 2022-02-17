02fd4928dae80159552106cd9ee21afb026edd4f
evrolan <evrolan1404@gmail.com>
2022-02-17 15:11:31 +0200

    Render list of items using list.map(obj => {...})

--- little-tutorials/counter-app/src/components/counter.jsx
+++ little-tutorials/counter-app/src/components/counter.jsx
@@ -3,15 +3,19 @@ import React, { Component } from "react";
 class Counter extends Component {
 	state = {
 		count: 0,
+		tags: ["tag1", "tag2", "tag3"],
 	};
 
 	render() {
 		return (
 			<div>
-				<span style={{ fontSize: 10 }} className="badge bg-primary m-2">
-					{this.formatCount()}
-				</span>
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
+				<ul>
+					{this.state.tags.map((tag) => (
+						<li key={tag}>{tag}</li>
+					))}
+				</ul>
 			</div>
 		);
 	}
@@ -19,6 +23,12 @@ class Counter extends Component {
 	formatCount() {
 		const { count } = this.state;
 		return count === 0 ? "Zero " : count;
+	}
+
+	getBadgeClasses() {
+		let classes = "badge text-reset m-2 bg-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
 	}
 }
 
