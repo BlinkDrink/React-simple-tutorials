927ef39ae523b9f61c6a74d8c4565c62fc0e3f16
evrolan <evrolan1404@gmail.com>
2022-02-18 16:05:46 +0200

    Implement sorting on a selected column of the table. Extract some of the logic for the Movies table, now we have common components TableHeader and TableBody(reusable)

--- little-tutorials/cinema-service/src/components/movies.jsx
+++ little-tutorials/cinema-service/src/components/movies.jsx
@@ -5,6 +5,7 @@ import ListGroup from "./common/listGroup";
 import MoviesTable from "./moviesTable";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
+import _ from "lodash";
 
 class Movies extends Component {
 	state = {
@@ -12,11 +13,12 @@ class Movies extends Component {
 		genres: [],
 		pageSize: 4,
 		currentPage: 1,
+		sortColumn: { path: "title", order: "asc" },
 	};
 
 	// Once the component is rendered, didMount gets called and we set the state here
 	componentDidMount() {
-		const genres = [{ name: "All Genres" }, ...getGenres()];
+		const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
 
 		this.setState({ movies: getMovies(), genres });
 	}
@@ -44,9 +46,14 @@ class Movies extends Component {
 		this.setState({ selectedGenre: genre, currentPage: 1 });
 	};
 
+	handleSort = (sortColumn) => {
+		this.setState({ sortColumn });
+	};
+
 	render() {
 		const { length: count } = this.state.movies;
-		const { pageSize, currentPage, selectedGenre, movies } = this.state;
+		const { pageSize, currentPage, selectedGenre, movies, sortColumn } =
+			this.state;
 
 		if (count === 0) return <p>There are no movies in the database.</p>;
 
@@ -55,7 +62,9 @@ class Movies extends Component {
 				? movies.filter((m) => m.genre._id === selectedGenre._id)
 				: movies;
 
-		const moviesPerPage = paginate(filtered, currentPage, pageSize);
+		const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+		const moviesPerPage = paginate(sorted, currentPage, pageSize);
 
 		return (
 			<div className="row">
@@ -71,9 +80,11 @@ class Movies extends Component {
 						Showing {filtered.length} movies in the database.
 					</p>
 					<MoviesTable
-						movies={movies}
+						movies={moviesPerPage}
+						sortColumn={sortColumn}
 						onLike={this.handleLike}
 						onDelete={this.handleDelete}
+						onSort={this.handleSort}
 					/>
 					<Pagination
 						itemsCount={filtered.length}
