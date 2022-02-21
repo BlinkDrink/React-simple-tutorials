7f1bc4376748907038cc1d3424762aa863623c84
evrolan <evrolan1404@gmail.com>
2022-02-21 22:58:24 +0200

    Implement login and register pages. Now we have reusable form component that all other forms can extend by inheriting it.

--- little-tutorials/cinema-service/src/components/loginForm.jsx
+++ little-tutorials/cinema-service/src/components/loginForm.jsx
@@ -1,50 +1,28 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
-class LoginForm extends Component {
-	username = React.createRef();
-
-	// componentDidMount() {
-	// 	this.username.current.focus();
-	// }
-
-	handleSubmit = (e) => {
-		e.preventDefault();
-
-		const username = this.username.current.value;
+class LoginForm extends Form {
+	state = {
+		data: { username: "", password: "" },
+		errors: {},
 	};
+
+	schema = {
+		username: Joi.string().min(5).max(30).required().label("Username"),
+		password: Joi.string().min(5).max(30).required().label("Password"),
+	};
+
+	doSubmit = () => {};
 
 	render() {
 		return (
 			<div>
 				<h1>Login</h1>
 				<form onSubmit={this.handleSubmit}>
-					<div className="mb-3">
-						<label htmlFor="username" className="form-label">
-							Username
-						</label>
-						<input
-							autoFocus
-							ref={this.username}
-							id="username"
-							type="text"
-							className="form-control"
-							placeholder="Username"
-						/>
-					</div>
-					<div className="mb-3">
-						<label htmlFor="password" className="form-label">
-							Password
-						</label>
-						<input
-							type="password"
-							className="form-control"
-							id="password"
-							placeholder="Password"
-						/>
-					</div>
-					<button type="submit" className="btn btn-primary">
-						Login
-					</button>
+					{this.renderInput("username", "Username")}
+					{this.renderInput("password", "Password", "password")}
+					{this.renderButton("Login")}
 				</form>
 			</div>
 		);
